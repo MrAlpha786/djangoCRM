4a4a513603c7fc60e629e6ed9a2563e96c6da2c7
Muhammad Faizan <Mr.Faizan007@pm.me>
2023-10-18 04:19:32 +0530

    add and delete records

--- src/frontend/views.py
+++ src/frontend/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
-from frontend.forms import SignUpForm
+from frontend.forms import AddRecordForm, SignUpForm
 from frontend.models import Record
 
 
@@ -62,4 +62,28 @@ def customer_record(request, pk, *args, **kwargs):
         return render(request, 'record.html', {'customer_record': customer_record})
 
     messages.error(request, "You should be logged in to view that page...")
+    return redirect(home)
+
+
+def delete_record(request, pk, *args, **kwargs):
+    if request.user.is_authenticated:
+        Record.objects.get(id=pk).delete()
+        messages.success(request, "Record deleted successfully.")
+        return redirect(home)
+
+    messages.error(request, "You must be logged in to do that...")
+    return redirect(home)
+
+
+def add_record(request, *args, **kwargs):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Record added...")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+
+    messages.success(request, "You must be logged in to do that...")
     return redirect(home)
